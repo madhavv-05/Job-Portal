978e699b5effa10533471a5515ea8f8377a66751
madhavguptaa <madhav87308@gmail.com>
2025-07-03 18:40:40 +0530

    settings.MEDIA_URL added

--- core/urls.py
+++ core/urls.py
@@ -4,6 +4,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
+    
     path('admin/', admin.site.urls),
     path('accounts/', include('apps.accounts.urls')),
     path('dashboard/', include('apps.dashboard.urls')),
